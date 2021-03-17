9cf4afea21e1695d08d37037971e56a9ce8f8f15
Aderinsola7 <78368015+Aderinsola7@users.noreply.github.com>
2021-03-17 05:26:37 -0700

    Add files via upload

---
+++ HealthProfile.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComputerizationHealthRecords
+{
+    class HealthProfile
+    {
+        private int _patientYearOfBirth;
+        private string _patientGender;
+
+        public HealthProfile(string firstname, string lastname, int yearofbirth, string gender, int height, int weight  )
+        {
+            PatientFirstName = firstname;
+            PatientlastName = lastname;
+            PatientYearOfBirth = yearofbirth;
+            PatientGender = gender;
+            PatientHeight = height;
+            PatientWeight = weight;
+
+        }
+
+        public string PatientFirstName { get; set; }
+        public string PatientlastName { get; set; }
+        public int PatientHeight { get; set; }
+        public int PatientWeight { get; set; }
+
+        public int PatientYearOfBirth
+        {
+            get
+            {
+                return _patientYearOfBirth;
+            }
+            set
+            {
+                while (value < 2001)
+                {
+                    Console.WriteLine("invalid year of birth ,please inter the correct year of birth: ");
+                    value = Convert.ToInt32(Console.ReadLine());
+                }
+
+                _patientYearOfBirth = value;
+            }
+        }
+
+        public string PatientGender
+        {
+            get
+            {
+                return _patientGender;
+            }
+            set
+            {
+                while (value.ToUpper() != "M" && value.ToUpper() != "F") ;
+                {
+                    Console.WriteLine("Please enter the correct gender (M or F)");
+                    value = Console.ReadLine();
+                }
+
+
+                _patientGender = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return DateTime.Today.Year - _patientYearOfBirth;
+            }
+        }
+
+        public int MaxHeartRate
+        {
+            get
+            {
+                return 220 - Age;
+            }
+        }
+        public int MaxTargetRate
+        {
+            get
+            {
+                return (Convert.ToInt32(MaxHeartRate * 0.92));
+            }
+        }
+        public int MinTargetRate
+        {
+            get
+            {
+                return (Convert.ToInt32(MaxHeartRate * 0.7));
+            }
+        }
+
+        public double BMI
+        {
+            get
+            {
+                return (PatientWeight / (Math.Pow(PatientHeight, 2))) * 703;
+            }
+        }
+
+        public string BMIEvaluation()
+        {
+            if (BMI < 18.5)
+                return "Underweight";
+            else if (BMI < 25)
+                return "Normal";
+            else if (BMI < 30)
+                return "Overweight";
+            else
+                return "Obese";
+        }
+    }
+}
---
+++ computerized health record.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ComputerizationHealthRecords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name? ");
+            var firstname = Console.ReadLine();
+            Console.WriteLine("What is your last name? ");
+            var lastname = Console.ReadLine();
+            Console.WriteLine("What is your year of birth? ");
+            var yearofbirth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your gender? ('M' or 'F') ");
+            var gender = Console.ReadLine();
+            Console.WriteLine("What is your height in inches? ");
+            var height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is your weight in pounds? ");
+            var weight = Convert.ToInt32(Console.ReadLine());
+
+            var healthProfile = new HealthProfile(firstname, lastname, yearofbirth, gender, height, weight);
+
+            Console.WriteLine("Name: {0} {1}", healthProfile.PatientFirstName, healthProfile.PatientlastName);
+            Console.WriteLine("Year of birth: {0}",healthProfile.PatientYearOfBirth);
+            Console.WriteLine("Age: {0}", healthProfile.Age);
+            Console.WriteLine("Gender: {0}", healthProfile.PatientGender.ToUpper() == "M"? "Male" : "Female");
+            Console.WriteLine("Max safe heart rate: {0}. Workout heart rate target range {1} to {2}", healthProfile.MaxHeartRate, healthProfile.MinTargetRate, healthProfile.MaxTargetRate);
+            Console.WriteLine("Height: {0}\". Weight {1} 1bs",healthProfile.PatientHeight ,healthProfile.PatientWeight);
+            Console.WriteLine("BMI: {0:##.##}%, Which means you are {1}", healthProfile.BMI, healthProfile.BMIEvaluation());
+            Console.ReadLine();
+            
+        }
+    }
+}
